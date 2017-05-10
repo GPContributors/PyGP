712818dbd987559b898354799a3401af680e14f2
GPContributors <gpcontributors@gmail.com>
2017-05-10 16:41:14 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import sys
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return MagicMock()
+
+MOCK_MODULES = ['_scard']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
 import sys, os
 import sphinx_rtd_theme
 sys.path += ['../src']
